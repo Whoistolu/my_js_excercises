6a23063216ca06fd83f889de7975fcc56234a7a3
Whoistolu <digittolu25@gmail.com>
2021-08-01 21:47:58 +0100

    add new lines

--- structures_and_algorithms/sum.js
+++ structures_and_algorithms/sum.js
@@ -1,8 +1,12 @@
 function sumAll(arr) {
     let start = Math.min(arr[0], arr[1]);
     let end = Math.max(arr[0], arr[1]);
+    let total = 0;
 
-    console.log(start, end)
+    for (var i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 }
 
 sumAll([1, 4]);
