02ae3aacd93e7693b198e1558f2bda04ea29f5b6
Whoistolu <digittolu25@gmail.com>
2021-08-03 00:07:19 +0100

    add reverse a string solution

---
+++ reverse-a-string/reverse.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    let strArr = str.split("");
+    let reverseStrArr = strArr.reverse();
+    let reversedStr = reverseStrArr.join("");
+    return reverseString;
+}
+console.log(reverseString("hello"));
