c33fdc3d86e5a044f43be6da6e720092ab1c115e
Whoistolu <digittolu25@gmail.com>
2021-08-09 22:31:15 +0100

    add new update

--- Palindrome-Check/palindrome.js
+++ Palindrome-Check/palindrome.js
@@ -1,6 +1,8 @@
-function (str) {
+function palindrome(str) {
     let reversed = str.split("").reversed().join("");
-    if (reversed === str) {
+    if (reversed === str) return true;
 
-    }
+    return false;
 }
+
+palindrome("racecars");
