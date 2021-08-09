91938b4cfa00bec93cf5e86e213b96528bc1d3ef
Whoistolu <digittolu25@gmail.com>
2021-08-09 22:33:16 +0100

    add new update

--- Palindrome-Check/palindrome.js
+++ Palindrome-Check/palindrome.js
@@ -1,4 +1,7 @@
 function palindrome(str) {
+    /*Using regex*/
+    /**Will complete this exercise tomorrow*/
+
     let reversed = str.split("").reversed().join("");
     if (reversed === str) return true;
 
