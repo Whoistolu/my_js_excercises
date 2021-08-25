b78cca5ebca7267c050af77be9d7345a0d43670c
Whoistolu <digittolu25@gmail.com>
2021-08-25 20:52:43 +0100

    Add newsolution alternative

--- Palindrome-Check/palindrome.js
+++ Palindrome-Check/palindrome.js
@@ -9,3 +9,13 @@ function palindrome(str) {
 }
 
 palindrome("racecars9");
+
+/***or**/
+
+const palindrome = str => {
+    // turn the string to lowercase
+    str = str.toLowerCase()
+    // reverse input string and return the result of the
+    // comparisong
+    return str === str.split('').reverse().join('')
+  }
