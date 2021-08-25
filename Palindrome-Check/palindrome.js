9347666d452cc790acb0f3adfa2845d7e359138c
Whoistolu <digittolu25@gmail.com>
2021-08-25 20:57:42 +0100

    Fix  issue

--- Palindrome-Check/palindrome.js
+++ Palindrome-Check/palindrome.js
@@ -14,8 +14,8 @@ palindrome("racecars9");
 
 const palindrome = str => {
     // turn the string to lowercase
-    str = str.toLowerCase()
+    str = str.toLowerCase();
     // reverse input string and return the result of the
     // comparisong
-    return str === str.split('').reverse().join('')
+    return str === str.split('').reverse().join('');
   }
